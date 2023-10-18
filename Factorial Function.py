5830a073ddf929aac18051c9369d0db31fe1d1de
Mobin Seyedjafary <Mobinof.sj@gmail.com>
2023-10-18 14:44:17 +0330

    Change part 1

--- Factorial Function.py
+++ Factorial Function.py
@@ -1,4 +1,10 @@
 def factorial(num):
+    if type(num) is not int:
+        raise TypeError('The number must be an integer')
+    if num < 0:
+        raise ValueError('The number must be positive')
+    if num == 0:
+        return 1
     result = 1
     while num > 0:
         result *= num
